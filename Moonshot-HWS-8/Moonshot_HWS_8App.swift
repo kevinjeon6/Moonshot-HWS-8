57ed2e59c62b5dfd1635103edc64384a5aae4620
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-11-14 19:57:27 -0500

    Initial Commit

---
+++ Moonshot-HWS-8/Moonshot_HWS_8App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_HWS_8App.swift
+//  Moonshot-HWS-8
+//
+//  Created by Kevin Mattocks on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_HWS_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
