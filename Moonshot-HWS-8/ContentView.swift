3e600f81d0098f21602cb2a46f67059a4ebb2780
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-11-15 21:32:52 -0500

    Learn how ScrollView lets us work with data Learn that a Lazy Vstack or Hstack will make the view when shown and instantly Learn that you can change the ScrollView direction

--- Moonshot-HWS-8/ContentView.swift
+++ Moonshot-HWS-8/ContentView.swift
@@ -10,15 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
        
-        GeometryReader { geo in
-            Image("ghost")
-                //Resizeable tells SwiftUI to make the image scalable and stretchable within the frame
-                .resizable()
-                .scaledToFill()
-                .frame(width: geo.size.width * 0.8, height: 350)
-            //geo.size.width * 0.8 means you want to take up 80% of our available screen width
-                .frame(width: geo.size.width, height: geo.size.height)
-            //Add a second frame if you want to center a view inside a GeometryReader
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
        
     }
